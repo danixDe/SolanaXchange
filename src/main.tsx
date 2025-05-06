799d3a884414c53042805067b40f4a2617d69d56
danixDe <150506676+danixDe@users.noreply.github.com>
2025-05-07 01:37:18 +0530

    xx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Buffer } from 'buffer';
+import process from 'process';
 import {BrowserRouter as Router} from 'react-router-dom'
-;(window as any).Buffer = Buffer;
-createRoot(document.getElementById('root')!).render(
+if (!window.Buffer) window.Buffer = Buffer;
+if (!window.process) window.process = process;createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
     <Router>
     <App />
     </Router>
   </StrictMode>,
+  
 )
--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
-      buffer: 'buffer', 
-      process: 'process/browser',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
     },
   },
   define: {
-    'process.env': {},
+    global: 'globalThis', 
+    'process.env': {},    
   },
   optimizeDeps: {
     include: ['buffer', 'process'],
   },
   build: {
     rollupOptions: {
-      plugins: [rollupNodePolyFill()], 
+      plugins: [rollupNodePolyFill()],
     },
   },
 });
